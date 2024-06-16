8e273c6afaae2108c3941f4f4cc0bac8f73de7ca
pdr0-42 <pviniciuspsilva@gmail.com>
2024-06-15 23:44:41 -0300

    FEATURE: adding retriver

--- app/model/retriever.py
+++ app/model/retriever.py
@@ -1,3 +1,20 @@
-from langchain.document_loaders import PyPDFLoader
+import openai
+from utilities.config import config
+from llm_connector import OpenAIConnector
 
-loader  = PyPDFLoader()
+class RetrieverAugmentedGenerate:
+    def __init__(self) -> None:
+        self.llm = OpenAIConnector(
+            api_key=config.openai_api_key,
+            llm_model=config.openai_model
+            )
+        self._vector_store_id = config.openai_vector_store
+        self._model = config.openai_vector_store
+
+    def get_embeddings(self, text: str):
+        """"""
+        response = openai.embeddings.create(
+            model=self.llm.llm_model,
+            input=text
+        )
+        return response['data'][0]['embeddings']
